59727603bc3450cc94622dd1eb2fca79e19b1f6c
szrharrison <s.z.r.harrison@gmail.com>
2017-11-20 14:34:42 -0500

    Implemented spherical movement. There is a jump at start of animation that needs to be fixed.

--- src/SphericalCoordinateHandler.js
+++ src/SphericalCoordinateHandler.js
@@ -8,8 +8,8 @@ function CartesianCoordinateHandler(x,y,z) {
 }
 CartesianCoordinateHandler.toSpherical = function(x,y,z) {
   let radius = Math.sqrt(x**2 + y**2 + z**2),
-      polar = Math.acos(y / radius)  * (180 / Math.PI),
-      azimuthal = Math.asin(x / Math.sqrt(x**2 + z**2)) * (180 / Math.PI)
+      polar = Math.acos(y / radius),
+      azimuthal = Math.asin(x / Math.sqrt(x**2 + z**2))
 
   return {radius, polar, azimuthal}
 }
@@ -46,39 +46,123 @@ SphericalCoordinateHandler.prototype.update = function() {
   this.polar.rads = this.controls.getPolarAngle()
   this.polar.degs = this.polar.rads * (180 / Math.PI)
 }
-SphericalCoordinateHandler.prototype.toVector = function(rho, azimuthal, polar) {
+SphericalCoordinateHandler.prototype.toVector = function(radius, azimuthal, polar) {
   let x, y, z
-  if(rho !== undefined && azimuthal !== undefined && polar !== undefined) {
-    x = rho * Math.sin(azimuthal) * Math.sin(polar)
-    y = rho * Math.cos(polar)
-    z = rho * Math.sin(polar) * Math.cos(azimuthal)
+  if(radius !== undefined && azimuthal !== undefined && polar !== undefined) {
+    x = radius * Math.sin(azimuthal) * Math.sin(polar)
+    y = radius * Math.cos(polar)
+    z = radius * Math.sin(polar) * Math.cos(azimuthal)
   } else {
     this.update()
     x = this.radius * Math.sin(this.azimuthal.rads) * Math.sin(this.polar.rads),
     y = this.radius * Math.cos(this.polar.rads),
     z = this.radius * Math.sin(this.polar.rads) * Math.cos(this.azimuthal.rads)
-    console.log({x,y,z})
-    console.log(CartesianCoordinateHandler.toSpherical(x,y,z))
-    console.log({radius: this.radius, polar: this.polar.degs, azimuthal: this.azimuthal.degs})
-    console.log(this.controls.object.position)
-    console.log(this.controls.object.quaternion)
   }
+  return {x,y,z}
 }
 SphericalCoordinateHandler.prototype.travel = function(to, from) {
   this.update()
-  console.log('travel was called:')
+  const cos = Math.cos,
+        sin = Math.sin,
+        acos = Math.acos,
+        asin = Math.asin,
+        atan2 = Math.atan2,
+        tan = Math.tan,
+        PI = Math.PI
+  console.log('Travel was called:')
   if(from === undefined) {
-    from = this.position
+    from = {
+      vector: this.vector,
+      quaternion: this.quaternion
+    }
   }
-  const start = from.clone(),
-        scope = this
-  let t = 0
+  let t = 0,
+      currentSpherical = CartesianCoordinateHandler.toSpherical(from.vector.x, from.vector.y, from.vector.z),
+      nextSpherical = CartesianCoordinateHandler.toSpherical(to.vector.x, to.vector.y, to.vector.z),
+      R = this.radius,
+      polar1 = currentSpherical.polar,
+      polar2 = nextSpherical.polar,
+      polarDelta = polar2 - polar1,
+      azimuthal1 = currentSpherical.azimuthal,
+      azimuthal2 = nextSpherical.azimuthal,
+      azimuthalDelta = azimuthal2 - azimuthal1,
+      a = Math.sin(azimuthalDelta/2) * Math.sin(azimuthalDelta/2) +
+          Math.cos(azimuthal1) * Math.cos(azimuthal2) *
+          Math.sin(polarDelta/2) * Math.sin(polarDelta/2),
+      c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a))
+
+  c = Math.round(c * 100000) / 100000
+
+  const startQ = from.quaternion.clone(),
+        scope = this,
+        chord = to.vector.clone().sub(from.vector),
+        unitChord = {
+          '∆x': chord.x / R, //cos(polar2)*sin(azimuthal2) - cos(polar1)*sin(azimuthal1),
+          '∆y': chord.y / R, // cos(polar2)*cos(azimuthal2) - cos(polar1)*cos(azimuthal1),
+          '∆z': chord.z / R // sin(polar2) - sin(polar1)
+        },
+        chordLength = Math.sqrt(unitChord['∆x'] ** 2 + unitChord['∆y'] ** 2 + unitChord['∆z'] ** 2),
+        centralAngle = 2 * Math.asin(chordLength / 2)
+  console.log('length:', chordLength)
+  console.log('angle:', centralAngle * (180 / PI))
+  console.log('Other angle:', c * (180 / PI))
+  console.log('chord deltas:', {'∆x': chord.x, '∆y': chord.y, '∆z': chord.z})
+  console.log('unitChord deltas:', {'∆x': unitChord['∆x'] * R, '∆y': unitChord['∆y'] * R, '∆z': unitChord['∆z'] * R})
+  console.log({
+    polar1: polar1 * (180 / Math.PI),
+    polar2: polar2 * (180 / Math.PI)
+  }, {
+    azimuthal1: azimuthal1 * (180 / Math.PI),
+    azimuthal2: azimuthal2 * (180 / Math.PI)
+  })
+  let fromX = from.vector.x,
+      fromY = from.vector.y,
+      fromZ = from.vector.z
   function step() {
-    t += 0.05
+
     let stepQ = new Quaternion(),
-        stepV = new Vector3
-    Quaternion.slerp(start, to.quaternion, stepQ, t)
-    from.set(stepQ.x,stepQ.y,stepQ.z,stepQ.w)
+        stepV = new Vector3(),
+        u, v,
+        x = from.vector.x,
+        y = from.vector.y,
+        z = from.vector.z
+    if(c !== 0) {
+      let pDelt = polarDelta * t,
+          p = polar1 + pDelt,
+          aDelt = azimuthalDelta * t,
+          a = azimuthal1 + aDelt,
+          v = scope.toVector(R, a, p)
+
+      x = v.x
+      y = v.y
+      z = v.z
+      // u = sin((1 - t) * centralAngle) / sin(centralAngle)
+      // v = sin(t * centralAngle) / sin(centralAngle)
+      // y = u * cos(polar1) * cos(azimuthal1) +
+      //     v * cos(polar2) * cos(azimuthal2)
+      // z = u * sin(polar1) * cos(azimuthal1) +
+      //     v * sin(polar2) * cos(azimuthal2)
+      // x = u * sin(azimuthal1) + v * sin(azimuthal2)
+      // // x = Math.round(x * R * 1000) / 1000
+      // // y = Math.round(y * R * 1000) / 1000
+      // // z = Math.round(z * R * 1000) / 1000
+      // x = x * R
+      // y = y * R
+      // z = z * R
+    }
+
+    console.log('     t =', t)
+    console.log('   from:', {
+      x: fromX,
+      y: fromY,
+      z: fromZ
+    })
+    console.log('current:', {x,y,z})
+    console.log('\n')
+    t += 0.05
+    Quaternion.slerp(startQ, to.quaternion, stepQ, t)
+    from.quaternion.set(stepQ.x,stepQ.y,stepQ.z,stepQ.w)
+    from.vector.set(x,y,z)
     if(t < 1) {
       requestAnimationFrame(step)
     }
